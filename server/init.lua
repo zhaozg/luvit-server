9ec60bdabc9e533fd9d23e6c4d31eaec9ff2ca67
George Zhao <zhaozg@gmail.com>
2017-07-11 16:43:10 +0800

    update meta

--- server/init.lua
+++ server/init.lua
@@ -1,6 +1,6 @@
 return {
   name = "zhaozg/server",
-  version = "0.0.6",
+  version = "0.0.7",
   homepage = "https://github.com/zhaozg/luvit-server",
   description = "general http server for luvit.",
   tags = {"httpd", "server", "luvit"},
@@ -10,7 +10,7 @@ return {
     "**.lua",
   },
   dependencies = {
-    "zhaozg/haml@0.3.6",
+    "zhaozg/haml@0.3.7",
     "zhaozg/http-cookie@0.1.3"
   }
 }
